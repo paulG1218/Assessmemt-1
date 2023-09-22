18bf7c6b502cf3bc04571ed2bbc303f57396bfd4
Paul Gerritsen <paul188297@gmail.com>
2023-09-22 14:47:33 -0600

    solved first 2 further study

--- src/further-study.js
+++ src/further-study.js
@@ -6,14 +6,37 @@
 //   => ['a']
 //   commonStrings(['zoo', 'space', 'zoo'], ['zoo', 'space', 'boat']);
 //   => ['zoo', 'space']
-function commonStrings(array1, array2) {}
+function commonStrings(array1, array2) {
+    const array3 = []
+    for (let item of array1) {
+        if (array2.includes(item)) {
+            if (array3.includes(item)) {}
+            else {
+            array3.push(item)
+            }
+        }
+    }
+    return array3
+}
 
 // Given three numbers, return a list of numbers from 1 to 100 that are
 // divisible by at least one of a, b or c.
 // Ex.:
 //   divisibleByEither(50, 30, 29);
 //   => [29, 30, 50, 58, 60, 87, 90, 100]
-function divisibleByEither(a, b, c) {}
+function divisibleByEither(a, b, c) {
+    const div = []
+    for(let i = 1; i <= 100; i ++) {
+        if (Number.isInteger(i / a)) {
+            div.push(i)
+        } else  if (Number.isInteger(i / b)) {
+            div.push(i)
+        } else  if (Number.isInteger(i / c)) {
+            div.push(i)
+        } else {}
+    }
+    return div
+}
 
 // Compress a string using the rules below and return the result. To compress a
 // string, replace consecutive duplicate characters with a number and the
